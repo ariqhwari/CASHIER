84d52360d112c472f35a06adad97ba34e2696fee
fildzahattira <fildzahattira560@gmail.com>
2023-05-21 19:24:29 +0700

    first commit

---
+++ FormInsertCategory.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FormInsertCategory</title>
+</head>
+<body>
+    <h2>Category</h2>
+            <table>
+                    <form action = "InsertCategory.php" method = "POST">
+                        <tr>
+                            <td>
+                                <label for = "name_category">
+                                    Category name :
+                                </label>
+                            </td>
+                            <td>
+                                <input type = "text" name = "name_category"/>
+                            </td>
+                        </tr>
+                    <p></p>
+                        <tr>
+                            <td>
+                                <input type="submit" name="submit" value="Submit">   
+                                <input type = "reset" value = "Reset"/>
+                            </td>
+                        </tr>
+                    </form>
+</body>
+</html>
