1fedc0cb2d8427aa3405c6086213299fc0043598
fildzahattira <fildzahattira560@gmail.com>
2023-05-21 14:53:24 +0700

    first commit

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php
+$mysqli = new mysqli("localhost","root","","dbshop","3307");
+
+// Periksa Koneksi
+if($mysqli -> connect_errno) {
+  echo "Gagal menghubungkan ke MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+// echo "connection : success";
+//echo "<hr />";
+?>
