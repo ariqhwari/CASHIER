b8403e737ce551bbd43788dd07846cb8c89a7d5f
ChurriAsna <121346169+ChurriAsna@users.noreply.github.com>
2023-05-22 23:51:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" href="FP/custom.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 mt-5 bg-white shadow p-5">
+                <h4 class="text-center" style="color:#7D1A1A"><b>$mallCashier</b></h4>
+                <div class="text-center"><img src="assets/logo.png" width="120"></div>
+                    <form mthode="POST"action="">
+                        <div class="mb-3">
+                            <label>E-mail</label>
+                            <input type="email" name="email" class="form-control" placeholder="E-mail" style="background-color:#BD8B8B">
+                        </div>
+                        <div class="mb-3">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" placeholder="Password" style="background-color:#BD8B8B">
+                        </div>
+                        <button class="btn btn-primary" name="masuk" style="background-color:#7D1A1A">masuk</button>
+                        
+                    </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
