e66f03a8bcc6138a0d91c9a29450e9c68f172fe0
Mutya Anissa Maulinda <125990165+MutyaAnissa@users.noreply.github.com>
2023-05-24 10:07:54 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,8 @@
+<div class ="card border-0 shadow">
+    <div class ="card-body">
+        <h6>Selamat Datang <?php echo $_SESSION['USER']["nama_user"] ?></h6>
+        <p>
+            Kelola data produk dan detail penjualan melalui panel ini.
+        </p>
+    </div>
+</div>
