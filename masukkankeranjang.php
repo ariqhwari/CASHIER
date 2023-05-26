292903bccc1e7a3fcda2b50329400182ae6807f9
asafirstha <125427397+asafirstha@users.noreply.github.com>
2023-05-26 21:34:53 +0700

    masukkankeranjang.php

--- masukkankeranjang.php
+++ masukkankeranjang.php
@@ -1,14 +1,14 @@
 <?php
 include 'conn.php';
-$id_produk = $_POST['id_produk'];
+$id_product = $_POST['id_product'];
 
 //jika keranjang belum ada id_product, maka jumlahnya default(1)
-if (!isset($_SESSION['keranjang'][$id_produk]))
+if (!isset($_SESSION['keranjang'][$id_product]))
 {
-    $_SESSION['keranjang'][$id_produk]=1;
+    $_SESSION['keranjang'][$id_product]=1;
 }
 else
 {
-    $_SESSION['keranjang'][$id_produk] +=1;
+    $_SESSION['keranjang'][$id_product] +=1;
 }
 ?>
