f3d5619a4f5b5b7ace0eeb33cd8803814c298153
fildzahattira <fildzahattira560@gmail.com>
2023-05-29 09:09:04 +0700

    edit program

--- cashier/index.php
+++ cashier/index.php
@@ -27,7 +27,10 @@ if (!isset($_SESSION['user'])) {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="cashier/index.php">Home</a>
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="customer_cashier.php">Customer</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="penjualan.php">Orders</a>
