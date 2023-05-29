6feb192ae4adff0b4ff597b7851848b477de9c5f
ChurriAsna <121346169+ChurriAsna@users.noreply.github.com>
2023-05-29 20:41:05 +0700

    Add files via upload

---
+++ warehouse/product_delete.php
@@ -0,0 +1,21 @@
+<?php
+include '../conn.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+} else {
+    echo "ID tidak ditemukan.";
+    exit;
+}
+
+$query = "DELETE FROM product WHERE id_product=$id";
+
+if ($conn->query($query) === TRUE) {
+    echo "Data berhasil dihapus.";
+} else {
+    echo "Error: " . $query . "<br>" . $conn->error;
+}
+echo '<br><br><a href="product.php">Lihat Data</a>';
+
+$conn->close();
+?>
