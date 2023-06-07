95e7ea7ea088a5762c0429beb7c03a287f3296ed
asafirstha <asafirstha05@gmail.com>
2023-06-07 19:46:40 +0700

    update conn

--- conn.php
+++ conn.php
@@ -1,9 +1,3 @@
 <?php
 session_start();
 $conn = new mysqli("localhost", "root", "", "dbshop");
-
-// Periksa Koneksi
-if ($conn->connect_errno) {
-  echo "Gagal menghubungkan ke MySQL: " . $conn->connect_error;
-  exit();
-}
