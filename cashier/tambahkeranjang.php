bfaaa8d786a6af5f2fe03ba5f4dbfb377b577e58
asafirstha <125427397+asafirstha@users.noreply.github.com>
2023-06-08 00:12:44 +0700

    Add files via upload

---
+++ cashier/tambahkeranjang.php
@@ -0,0 +1,11 @@
+<?php
+include '../conn.php';
+$id_product = $_POST['id_product'];
+
+//jika keranjang belum ada id_product, maka jumlahnya default(1)
+if (!isset($_SESSION['keranjang'][$id_product])) {
+    $_SESSION['keranjang'][$id_product] = 1;
+} else {
+    $_SESSION['keranjang'][$id_product] += 1;
+}
+?>
